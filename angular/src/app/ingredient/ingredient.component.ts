8e338b4ae693017ce5e9e3d57b5ed0acec3eddfd
Max <enriquelongton@gmail.com>
2019-10-22 22:42:56 -0400

    started ingredient add, delete, edit, etc

--- angular/src/app/ingredient/ingredient.component.ts
+++ angular/src/app/ingredient/ingredient.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { IngredientService } from './ingredient.service'
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-ingredient',
   templateUrl: './ingredient.component.html',
   styleUrls: ['./ingredient.component.scss']
 })
 export class IngredientComponent implements OnInit {
-
-  ingredients: any[] = [
-    { name: 'chicken', unit_type: 'Poultry' },
-    { name: 'salt', unit_type: 'Dry' },
-    { name: 'pepper', unit_type: 'Dry' },
-    { name: 'tabasco', unit_type: 'Wet' },
-  ]
-  constructor() { }
+  modalRef: BsModalRef;
+  ingredients$ = this.ingredientService.ingredients$;
+  constructor(private ingredientService: IngredientService, private modalService: BsModalService) { }
 
   ngOnInit() {
+    this.ingredientService.getIngredients();
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
--- angular/src/app/ingredient/ingredient.service.ts
+++ angular/src/app/ingredient/ingredient.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: "root"
 })
 export class IngredientService {
+    ingredients$ = new BehaviorSubject<any>([])
     constructor(private http: HttpClient) { }
+
+
+    getIngredients() {
+        return this.http.get(`/api/ingredients/`).subscribe(result => {
+            this.ingredients$.next(result)
+        });
+    }
+
+    deleteIngredient(ingredient) {
+        return this.http.delete(`/api/ingredient/${ingredient.id}`).subscribe(result => {
+            console.log(result)
+        })
+    }
+
+    editIngredient(ingredient) {
+        return this.http.put(`/api/ingredient/${ingredient.id}`, ingredient).subscribe(result => {
+            console.log(result)
+        })
+    }
+
+    createIngredient(ingredient) {
+        return this.http.post(`/api/ingredients/`, ingredient).subscribe(result => {
+            console.log(result)
+        })
+    }
 }
