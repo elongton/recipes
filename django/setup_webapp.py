328612587379d744068c8013a62b54b65ab02407
Max <enriquelongton@gmail.com>
2019-11-02 14:39:47 -0400

    added docker files and docker compose

---
+++ django/setup_webapp.py
@@ -0,0 +1,15 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipeApi.settings')
+
+import django
+django.setup()
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username='elongton').exists(): #if the user does not exist
+    user = User.objects.create_superuser(username='elongton', email="", password='M3087puttie')
+    user.save()
+    print('(ADDED) ' + str(user))
+else:
+    user = User.objects.get(username='elongton')
+    print(str(user))
