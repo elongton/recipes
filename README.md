bf21bed2624e430567b664c3eab1aab62e193683
elongton <elongton@vt.edu>
2020-01-19 15:52:56 -0500

    updating scripts

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ docker container run --publish 80:80 --detach --name <container_name> <image_nam
 ## enter the container
 docker container exec -it <container_name> /bin/sh
 
+
+## build dockerfile and push to dockerhub
+
+
 ### stuff to come:
 
 1. Flavor ties - the app will find common flavors and pair them up with a roulette wheel
--- nginx/create_and_run.bat
+++ nginx/create_and_run.bat
@@ -1,4 +1,5 @@
 docker stop recipe_server
 docker rm recipe_server
 docker image build -t nginx_recipes .
-docker container run --publish 80:80 --detach --name recipe_server nginx_recipes:latest
+REM docker container run --publish 80:80 --detach --name recipe_server nginx_recipes:latest
+docker-compose -f docker-compose.yml up --build
